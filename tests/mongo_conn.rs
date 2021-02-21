3c71b5905f4c0632725602b9b77f982332e6f992
Nathan Leniz <nathan.leniz@mongodb.com>
2021-02-21 13:29:24 -0800

    add mongodb test

---
+++ tests/mongo_conn.rs
@@ -0,0 +1,16 @@
+use mongodb::Client;
+use std::env;
+
+#[actix_rt::test]
+async fn mongo_connection_works() {
+    let uri = env!("MONGODB_URI");
+    // let uri = env::var("MONGODB_URI").expect("no mongodb uri");
+    let client = Client::with_uri_str(&uri)
+        .await
+        .expect("couldn't connect to mongodb");
+    let dbs = client
+        .list_database_names(None, None)
+        .await
+        .expect("couldn't list database names");
+    assert!(&dbs.contains(&"develop".into()));
+}
